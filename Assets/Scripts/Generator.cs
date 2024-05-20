1be158e39295e2bf446f6f7523d779b916f28395
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-05-21 02:03:51 +0900

    [update] ベクトル便利

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -5,13 +5,52 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    private GameObject _obj;
-    private Mesh _mesh;
+    [SerializeField] private GameObject dividedTarget; // the target to divide
+    [SerializeField] private Vector2 divisionCount; // the division count x-y axis
 
     void Start()
     {
-        _obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-        _mesh = _obj.GetComponent<MeshFilter>().mesh;
+        // MakePyramid();
+
+        var mesh = dividedTarget.GetComponent<MeshFilter>()
+            .mesh;
+
+        List<Vector3> verts = new List<Vector3>();
+        mesh.GetVertices(verts);
+
+        Debug.Log($"Middle : {FindMiddle(verts)}");
+    }
+
+    // 縦の辺の中点 頂点の中で最も高い位置と最も低い位置のデータを参照している
+    private float FindMiddle(List<Vector3> verts)
+    {
+        float max, min;
+        // テキトーに初期化
+        max = verts[verts.Count - 1].y;
+        min = verts[0].y;
+
+        for (int i = 0; i < verts.Count; i++)
+        {
+            // 最大値
+            if (verts[i].y > max)
+            {
+                max = verts[i].y;
+            }
+
+            // 最小値
+            if (verts[i].y < min)
+            {
+                min = verts[i].y;
+            }
+        }
+
+        var v = (min - max) / 2f;   // 最高点から中点へのベクトル = 最高点から最低点へのベクトル / 2
+        return max + v;
+    }
+
+    private void MakePyramid()
+    {
+        var obj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
 
         Mesh nMesh = new Mesh();
         Vector3[] verts = new[]
@@ -28,13 +67,13 @@ public class Generator : MonoBehaviour
             0, 2, 1, // left
             1, 2, 3, // below
         };
-        
+
         nMesh.SetVertices(verts);
         nMesh.SetTriangles(triangles, 0);
         nMesh.RecalculateNormals();
         nMesh.RecalculateBounds();
         nMesh.RecalculateTangents();
-        _obj.GetComponent<MeshFilter>().mesh = nMesh;
+        obj.GetComponent<MeshFilter>().mesh = nMesh;
     }
 
     void Update()
